fc01d24a4612f69d120d27b248fd82c0e5500be1
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-04-29 22:16:23 +0530

    Create Hello world.java

---
+++ Hello world.java
@@ -0,0 +1,37 @@
+/* via regular way to perorm hello world*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+/*for printing in multiline*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World.");
+        System.out.println("I");
+        System.out.println("am");
+        System.out.println("coding");
+   
+    }
+}
+
+/* for printing in multiline but typed in single line*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("hello\nworld\nI\nam\ncoding");
+    }
+}
