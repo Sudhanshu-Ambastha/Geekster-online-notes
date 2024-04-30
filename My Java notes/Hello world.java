febfe7e7cd9a8ee0d51262060c763032cf4183ea
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-04-30 21:27:22 +0530

    Update Hello world.java

--- My Java notes/Hello world.java
+++ My Java notes/Hello world.java
@@ -1,7 +1,7 @@
 /* via regular way to perform hello world*/
 import java.io.*;
 import java.util.*;
-
+/* code alys goes top to bottm & left to right*/
 public class Solution {
 
     public static void main(String[] args) {
@@ -33,5 +33,25 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println("hello\nworld\nI\nam\ncoding");
+    }
+}
+
+// for writing '+' or '-' 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     Scanner scn = new Scanner(System.in);
+        
+      int x=scn.nextInt();
+      int y=scn.nextInt();
+        
+      int sum = x + y;
+      int diff = x - y;
+        
+      System.out.println(sum);
+      System.out.println(diff);
     }
 }
