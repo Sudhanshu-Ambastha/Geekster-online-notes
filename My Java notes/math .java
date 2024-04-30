ba1e15763e2cbb21c974ed81a5e32244144a88f8
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-04-30 22:58:58 +0530

    Update math .java

--- My Java notes/math .java
+++ My Java notes/math .java
@@ -97,3 +97,34 @@ public class Solution {
         System.out.println(ans);
     }
 }
+
+//Area of circle
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int d = scn.nextInt();
+        int r = d/2;
+        int r2= r*r;
+        int area = (22 * r2 / 7);
+        System.out.println(area);
+    }
+}
+
+//Area of Triangle
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double b = scn.nextDouble();
+        double h = scn.nextDouble();
+        double area = (b*h/2);
+        System.out.println(area);
+    }
+}
