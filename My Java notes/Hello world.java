0ab354a88a090725d076ab7037a43cc61b8deb92
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-04-30 17:35:18 +0530

    Update Hello world.java

--- My Java notes/Hello world.java
+++ My Java notes/Hello world.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.print("hello world");
     }
 }
 
