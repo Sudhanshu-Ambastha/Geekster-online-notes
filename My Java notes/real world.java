e3e32202a1299c71273da476aa59523105eeb03f
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-05-02 20:07:56 +0530

    Update real world.java

--- My Java notes/real world.java
+++ My Java notes/real world.java
@@ -1,1 +1,24 @@
 //Shop Discount
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.print("");
+        int units = scn.nextInt();
+        int unitCost = 100;
+        
+        int totalCost = units * unitCost;
+
+        if (totalCost > 1000) {
+            totalCost = (int) (totalCost * 0.9);
+        }
+
+        System.out.println("" + totalCost);
+    }
+}
+
+//
