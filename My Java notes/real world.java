d3eae51ceb6dfd78863403ff755c9a278dd89052
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-05-07 19:46:24 +0530

    Update real world.java

--- My Java notes/real world.java
+++ My Java notes/real world.java
@@ -548,34 +548,35 @@ public class Solution {
 //Switch Calculator 1
 import java.io.*;
 import java.util.*;
-
 public class OperationsBasedOnN {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int N = scanner.nextInt();
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
-        switch (N) {
-            case 10:
-                System.out.println(a + b);
-                break;
-            case 20:
-                System.out.println(a - b);
-                break;
-            case 30:
-                System.out.println(a * b);
-                break;
-            case 40:
-                System.out.println(a % b);
-                break;
-            case 50:
-                System.out.println(a / b);
-                break;
-            default:
-                System.out.println("");
-                break;
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        if (b > a || (N == 50 && b == 0)) {
+            System.out.println("Enter a valid number.");
+        } else {
+            switch (N) {
+                case 10:
+                    System.out.println(a + b);
+                    break;
+                case 20:
+                    System.out.println(a - b);
+                    break;
+                case 30:
+                    System.out.println(a * b);
+                    break;
+                case 40:
+                    System.out.println(a % b);
+                    break;
+                case 50:
+                    System.out.println(a / b);
+                    break;
+                default:
+                    System.out.println("Enter a valid number");
+                    break;
+            }
         }
     }
 }
