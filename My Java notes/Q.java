d024972258905fe59ca0332fcba0b13122dc8ab5
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-05-09 22:07:59 +0530

    Update Q.java

--- My Java notes/Q.java
+++ My Java notes/Q.java
@@ -126,3 +126,82 @@ public class Solution {
       }
   }
 }
+
+//HW_Basic Calculator 9
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt(); 
+        int y = scn.nextInt(); 
+        char ch = scn.next().charAt(0);
+
+        switch (ch) {
+            case 'A':
+                int sum = x + y;
+                System.out.println(sum);
+                break;
+            case 'S':
+                int difference = x - y;
+                System.out.println(difference);
+                break;
+            case 'M':
+                int product = x * y;
+                System.out.println(product);
+                break;
+            case 'D':
+                int division = x / y;
+                System.out.println(division);
+                break;
+            case 'R':
+                int remainder = x % y;
+                System.out.println(remainder);
+                break;
+            default:
+                System.out.println("Enter again");
+        }
+    }
+}
+
+//Concatenate_Two_Strings
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str1 =scn.nextLine();
+        String str2 =scn.nextLine();
+
+        String ans = str1 + str2;
+        System.out.println(ans);
+    }
+}
+
+//string concatenate 2
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str1 = scn.nextLine();
+        String str2 = scn.nextLine();
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        if( len1 > len2 ){
+         String ans = str2 + str1 + str2;
+         System.out.println(ans);
+        }else{
+         String ans = str1+ str2+ str1;
+         System.out.println(ans);
+        }
+    }
+}
