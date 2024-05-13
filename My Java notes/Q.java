deb558e7bc3c69f65d7e5a72113f5c3350d60850
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-05-13 21:59:20 +0530

    Update Q.java

--- My Java notes/Q.java
+++ My Java notes/Q.java
@@ -835,3 +835,22 @@ public class Solution {
         }
     }
 }
+
+// HW_Print series AAA,bb,CCC,dd,EEE,ff till 26 lines
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        for (int i = 1; i <= 26; i++) {
+            if (i % 2 == 1) {
+                System.out.println("" + ch + ch + ch);
+            } else {
+                System.out.println("" + (char)(ch + 32) + (char)(ch + 32));
+            }
+            ch++;
+        }
+    }
+}
