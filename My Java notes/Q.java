c9604b75e0684bdb450518a00b9c7eab0d2b3fef
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-05-16 23:17:14 +0530

    Update Q.java

--- My Java notes/Q.java
+++ My Java notes/Q.java
@@ -874,6 +874,30 @@ public class Solution {
     }
 }
 
+//Nth Fibonacci Number 7
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        int a = 0;
+        int b = 1;
+        int c;
+        int i = 0;
+        while(i < n){
+        c = a + b;
+        a = b;
+        b = c;
+         i++;
+        }
+       System.out.print(a);
+    }
+}
+
 //Print 4,13,22,31.....n
 import java.io.*;
 import java.util.*;
