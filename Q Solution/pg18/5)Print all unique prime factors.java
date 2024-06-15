cb75607f5ce8cee188bb14c515e5f86ed629eec8
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-06-15 11:54:28 +0530

    feat

--- Q Solution/pg18/5)Print all unique prime factors.java
+++ Q Solution/pg18/5)Print all unique prime factors.java
@@ -4,35 +4,27 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        scanner.close();
-
-        Set<Integer> primeFactors = getPrimeFactors(N);
-        for (int factor : primeFactors) {
-            System.out.println(factor);
-        }
-    }
-
-    private static Set<Integer> getPrimeFactors(int number) {
-        Set<Integer> primeFactors = new HashSet<>();
-        // Check for number of 2s that divide number
-        while (number % 2 == 0) {
-            primeFactors.add(2);
-            number /= 2;
-        }
-        // number must be odd at this point, so a skip of 2 (i = i + 2) can be used
-        for (int i = 3; i <= Math.sqrt(number); i += 2) {
-            // While i divides number, add i and divide number
-            while (number % i == 0) {
-                primeFactors.add(i);
-                number /= i;
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if (n % 2 == 0) {
+            System.out.println(2);
+            while (n % 2 == 0) {
+                n /= 2;
             }
         }
-        // This condition is to check if number is a prime number greater than 2
-        if (number > 2) {
-            primeFactors.add(number);
+        
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                System.out.println(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
         }
-        return primeFactors;
+        
+        if (n > 2) {
+            System.out.println(n);
+        }
     }
 }
