713ac0de40c8d80a41cfce08e2d1b217ea58f0e5
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-10-14 21:51:50 +0530

    ➕file

--- Module_04/script10.js
+++ Module_04/script10.js
@@ -11,3 +11,29 @@ console.log(rectangle);
 
 /*global execution context mcp = l, w, calculate, rectangle will run 1st then cep(code execution phase)will run
 */
+
+//First Unique Character in a String
+function getUniqueCharacters(input) {
+  let charCount = {};
+  let output = '';
+  
+  // Count frequency of each character
+  for (let char of input) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  
+  // Append characters that appear only once
+  for (let char of input) {
+    if (charCount[char] === 1) {
+      output += char;
+    }
+  }
+  
+  return output;
+}
+
+let input1 = "aakhhill";
+let input2 = "aakash";
+
+console.log(getUniqueCharacters(input1)); // Prints "ki"
+console.log(getUniqueCharacters(input2)); // Prints "kash"
